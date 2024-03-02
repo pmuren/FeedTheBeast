a877f59f9a9ffae049b57b4749addc139c58d55e
Paul Muren <paulmuren@gmail.com>
2024-03-01 22:47:21 -0500

    Who knows?

--- Player.gd
+++ Player.gd
@@ -2,7 +2,7 @@ extends Area2D
 
 signal hit
 
-export var speed = 400 # How fast the player will move (pixels/sec).
+@export var speed = 400 # How fast the player will move (pixels/sec).
 var screen_size # Size of the game window.
 
 
