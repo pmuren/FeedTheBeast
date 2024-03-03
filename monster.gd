6cf9e17119a4a49e5a22ece9eb35c1f781dc6363
Christopher Anderson <tophermichaelson@gmail.com>
2024-03-03 16:38:52 -0500

    animated monster added

--- monster.gd
+++ monster.gd
@@ -3,7 +3,7 @@ extends Area2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$AnimatedSprite2D.play('idle')
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -11,4 +11,11 @@ func _process(delta):
 	pass
 
 func _on_body_entered(body):
+	$AnimatedSprite2D.play('chewing')
+	$IdleSpriteTimer.start()
 	body.queue_free()
+
+
+
+func _on_idle_sprite_timer_timeout():
+	$AnimatedSprite2D.play('idle')
